1bca0e34bc88bd665977a512f695da2ee20d2c4b
Daniel Le and Gaurav <pair+daniel+gaurav@fastacash.com>
2013-06-26 17:53:10 +0800

    working version

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import java.rmi.RemoteException;
+
+import org.apache.axis2.AxisFault;
+
+import com.fastacash.vfx.MoBeLinkClassStub;
+import com.mobewallet.webservices.LogIn2Document;
+import com.mobewallet.webservices.LogIn2ResponseDocument;
+
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			MoBeLinkClassStub proxy = new MoBeLinkClassStub("http://www.vfxplc.com/mobe/mobelink.asmx");
+			LogIn2Document request = LogIn2Document.Factory.newInstance();
+			LogIn2Document.LogIn2 data = request.addNewLogIn2();
+			data.setClientID("mob900021");
+			data.setPassword("welcome");
+			data.setSourceSystem("fastacash");
+			
+			try {
+				LogIn2ResponseDocument response =  proxy.logIn2(request);
+//			response.get
+				System.out.println(response.toString());
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} catch (AxisFault e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
